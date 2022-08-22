c85c762187138755a1d4496a66dd41185ec856fb
Dmitry Shcherbin <dmitrysh@ntnu.no>
2022-08-22 11:59:18 +0200

    def check_exerciseid

--- actions_submit.py
+++ actions_submit.py
@@ -26,7 +26,74 @@ def get_column(sheet, row, name):
         print('Warning! Several cols with name '+name+ ' exiting')
         exit()
 
-  
+def check_exerciseid(wb_s,cursor,row):
+    #it checks wrapper and wrappereercise for id and wrapper_id and for exercise_id
+    sheet = wb_s['Lessons']
+    names,columns,ranges = get_sheet_structure(sheet = sheet)
+    
+    min_col, min_row,max_col,max_row=ranges[names.index('WrapperExercises')]
+    data = []
+    coord = []
+    ids = columns[names.index('WrapperExercises')]
+    for cells in sheet.iter_cols(min_col=min_col,min_row=row, max_col=max_col, max_row=row):
+        for cell in cells:
+            data.append(cell.value)
+            coord.append((cell.row,cell.column))
+    print(data, ids, coord)
+    
+    #compare wrapper id of an exercise with id of its parent folder from Wrappers columns    
+    min_col, min_row,max_col,max_row=ranges[names.index('Wrappers')]
+    for cells in sheet.iter_cols(min_col=min_col,min_row=2, max_col=max_col, max_row=2):
+        for cell in cells:
+            if cell.value == 'Id':
+                Wrapper_Id_column = cell.column
+    #search for parent wrapper id scrolling upward
+    finished = False
+    wrapper_id_row = row
+    while not finished:
+        wrapper_id_row = wrapper_id_row - 1
+        if sheet.cell(row=wrapper_id_row,column=Wrapper_Id_column).value:
+            finished = True
+            Wrapper_Id = sheet.cell(row=wrapper_id_row,column=Wrapper_Id_column).value
+    #check where whrapper_id coincides with wrapper id, if not, replace with parents id
+    if data[ids.index('Wrapper_Id')] == Wrapper_Id:
+        print(sheet.cell(row=coord[ids.index('Wrapper_Id')][0], column=coord[ids.index('Wrapper_Id')][1]).value,'here')
+    else:
+        sheet.cell(row=coord[ids.index('Wrapper_Id')][0], column=coord[ids.index('Wrapper_Id')][1]).value=Wrapper_Id
+    #check if it has an exercise id 
+    Create_Entry = False
+    if data[ids.index('Exercise_Id')]:
+        print('already exists the entry in the excel file, checking for existance in the database')
+        sqlcommand = 'SELECT * FROM [CalstContent].[dbo].[WrapperExercises] where Exercise_Id = ' + str(data[ids.index('Exercise_Id')]) + ' AND Wrapper_Id = '+str(Wrapper_Id)
+        cursor.execute(sqlcommand)
+        
+        list = cursor.fetchall()
+        if not list:
+            Create_Entry = True
+        else:
+            print('db entry exists')
+    else:
+        Create_Entry = True
+
+    if Create_Entry == True:
+        print('creating an entry')
+        cursor.execute('SELECT MAX(Id) AS maximum FROM Exercises')
+        Exercise_Id = cursor.fetchall()[0][0]+1
+        print(Exercise_Id)
+        sqlcommand = 'INSERT INTO [dbo].[WrapperExercises] ([Wrapper_Id],[Exercise_Id]) VALUES '
+        list = sqlcommand.split()[3].split(',')
+        print(list)
+        values = sqlcommand.split()[3].replace('[Wrapper_Id]',str(Wrapper_Id)).replace('[Exercise_Id]',str(Exercise_Id))
+        sqlcommand = sqlcommand + values
+        
+        cursor.execute(sqlcommand)
+        sheet.cell(row=coord[ids.index('Exercise_Id')][0], column=coord[ids.index('Exercise_Id')][1]).value = Exercise_Id
+        
+    else:
+        pass
+    return Create_Entry
+
+
 
 def main(folder,cursor):
     path = Path(folder)
@@ -35,7 +102,7 @@ def main(folder,cursor):
     firstrun = True
     if structure_path.exists():
         wb_s = load_workbook(str(structure_path))
-        sheet = wb_s.active
+        sheet = wb_s['Lessons']
         to_submit = []
         print(str(structure_path))
         action_col = get_column(sheet=sheet, row = 1, name='Actions')
@@ -51,73 +118,10 @@ def main(folder,cursor):
         
         
         for row in to_submit:
-            names,columns,ranges = get_sheet_structure(sheet = sheet)
-            print(names.index('WrapperExercises'))
-            min_col, min_row,max_col,max_row=ranges[names.index('WrapperExercises')]
-            data = []
-            coord = []
-            ids = columns[names.index('WrapperExercises')]
-            for cells in sheet.iter_cols(min_col=min_col,min_row=row, max_col=max_col, max_row=row):
-                for cell in cells:
-                    data.append(cell.value)
-                    coord.append((cell.row,cell.column))
-            print(data, ids, coord)
-            
-            #compare wrapper id of an exercise with id of its parent folder from Wrappers columns    
-            min_col, min_row,max_col,max_row=ranges[names.index('Wrappers')]
-            for cells in sheet.iter_cols(min_col=min_col,min_row=2, max_col=max_col, max_row=2):
-                for cell in cells:
-                    if cell.value == 'Id':
-                        Wrapper_Id_column = cell.column
-            #search for parent wrapper id scrolling upward
-            finished = False
-            wrapper_id_row = row
-            while not finished:
-                wrapper_id_row = wrapper_id_row - 1
-                if sheet.cell(row=wrapper_id_row,column=Wrapper_Id_column).value:
-                    finished = True
-                    Wrapper_Id = sheet.cell(row=wrapper_id_row,column=Wrapper_Id_column).value
-            #check where whrapper_id coincides with wrapper id, if not, replace with parents id
-            if data[ids.index('Wrapper_Id')] == Wrapper_Id:
-                print(sheet.cell(row=coord[ids.index('Wrapper_Id')][0], column=coord[ids.index('Wrapper_Id')][1]).value,'here')
-            else:
-                sheet.cell(row=coord[ids.index('Wrapper_Id')][0], column=coord[ids.index('Wrapper_Id')][1]).value=Wrapper_Id
-            #check if it has an exercise id 
-            Create_Entry = False
-            if data[ids.index('Exercise_Id')]:
-                print('already exists the entry in the excel file, checking for existance in the database')
-                sqlcommand = 'SELECT * FROM [CalstContent].[dbo].[WrapperExercises] where Exercise_Id = ' + str(data[ids.index('Exercise_Id')]) + ' AND Wrapper_Id = '+str(Wrapper_Id)
-                cursor.execute(sqlcommand)
-               
-                list = cursor.fetchall()
-                if not list:
-                    Create_Entry = True
-                else:
-                    print('db entry exists')
-            else:
-                Create_Entry = True
-            
-                
-
-
-            if Create_Entry == True:
-                print('creating an entry')
-                cursor.execute('SELECT MAX(Id) AS maximum FROM Exercises')
-                Exercise_Id = cursor.fetchall()[0][0]+1
-                print(Exercise_Id)
-                sqlcommand = 'INSERT INTO [dbo].[WrapperExercises] ([Wrapper_Id],[Exercise_Id]) VALUES '
-                list = sqlcommand.split()[3].split(',')
-                print(list)
-                values = sqlcommand.split()[3].replace('[Wrapper_Id]',str(Wrapper_Id)).replace('[Exercise_Id]',str(Exercise_Id))
-                sqlcommand = sqlcommand + values
-                
-                cursor.execute(sqlcommand)
-                sheet.cell(row=coord[ids.index('Exercise_Id')][0], column=coord[ids.index('Exercise_Id')][1]).value = Exercise_Id
+            if check_exerciseid(wb_s=wb_s, cursor=cursor, row=row):
                 wb_s.save(filename=(str(structure_path))) 
             else:
                 pass
-
-
 
           
                
