da95393d04e0acb7d936d406c05ef1244ad67554
Dmitry Shcherbin <dmitrysh@ntnu.no>
2022-08-29 19:21:21 +0200

    exercises sheet

--- actions_submit.py
+++ actions_submit.py
@@ -1,4 +1,5 @@
-from typing import ValuesView
+
+from ast import Index
 from natsort import natsorted
 from itertools import groupby
 from pathlib import Path
@@ -92,9 +93,151 @@ def check_exerciseid_in_structure(wb_s,cursor,row):
         pass
     return Create_Entry,Wrapper_Id,Exercise_Id
 
+def work_on_exercises_sheet(wb,cursor,cnxn, Wrapper_Id, Exercise_Id):
+    sheet = wb['Exercise']
+    Edit_Excel = False
+
+    names, columns,ranges = get_sheet_structure(sheet=sheet)
+    print(names, columns,ranges)
+    #check WrapperExercises values
+    min_col,min_row, max_col, max_row = ranges[names.index('WrapperExercises')]
+    ids = columns[names.index('WrapperExercises')]
+
+    values = []
+    for cells in sheet.iter_cols(min_col=min_col,min_row=3, max_col=max_col, max_row=3):
+        for cell in cells:
+            values.append((cell.value,cell.column))
+    print(values)
+    if values[ids.index('Wrapper_Id')][0] != Wrapper_Id:
+        print('wrapper_id', str(Wrapper_Id),values[ids.index('Wrapper_Id')][0])
+        sheet.cell(row=3, column=values[ids.index('Wrapper_Id')][1]).value=Wrapper_Id
+        Edit_Excel = True
+    else:
+        pass
+    if values[ids.index('Exercise_Id')][0] != Exercise_Id:
+        print('exercise_id', str(Exercise_Id),values[ids.index('Exercise_Id')][0])
+        sheet.cell(row=3, column=values[ids.index('Exercise_Id')][1]).value = Exercise_Id
+        Edit_Excel = True
+    else:
+        pass
+
+    #go to exercises column, change exercise id and make a db entry if needed
+    min_col,min_row, max_col, max_row = ranges[names.index('Exercises')]
+    ids = columns[names.index('Exercises')]
+
+    values = []
+    for cells in sheet.iter_cols(min_col=min_col,min_row=3, max_col=max_col, max_row=3):
+        for cell in cells:
+            values.append((cell.value,cell.column))
+    print(values)
 
 
-def main(folder,cursor):
+    if values[ids.index('Id')][0] != Exercise_Id:
+        print('exercise_id', str(Exercise_Id),values[ids.index('Id')][0])
+        data_row = 3
+        sheet.cell(row=data_row, column=values[ids.index('Id')][1]).value = Exercise_Id
+        Edit_Excel = True
+        #check whether entry exericse exists
+        
+    else:
+        pass
+
+    RendererId = values[ids.index('RendererId')][0]
+    print(RendererId)
+
+
+    sqlcommand = 'SELECT * FROM [CalstContent].[dbo].[Exercises] where Id = '+str(Exercise_Id)
+    #sqlcommand = 'INSERT INTO [dbo].[Exercises] ([Wrapper_Id],[Exercise_Id]) VALUES '
+    #list = sqlcommand.split()[3].split(',')
+    #values = sqlcommand.split()[3].replace('[Wrapper_Id]',str(Wrapper_Id)).replace('[Exercise_Id]',str(Exercise_Id))
+    #sqlcommand = sqlcommand + values
+    cursor.execute(sqlcommand)
+    list = cursor.fetchall()
+    Create_Entry = False
+    if not list:
+        Create_Entry = True
+        print(Create_Entry)
+    else:
+        print('db entry exists')
+        
+
+    if Create_Entry:
+        sqlcommand = 'INSERT INTO [dbo].[Exercises] ([Id],[RendererId]) VALUES '
+        list = sqlcommand.split()[3].split(',')
+        values = sqlcommand.split()[3].replace('[Id]',str(Exercise_Id)).replace('[RendererId]',str(RendererId))
+        sqlcommand = sqlcommand + values
+        print(sqlcommand)
+        cursor.execute(sqlcommand)
+
+
+    #fill in properties, check for exercise id and existence in the db
+    print(names)
+
+    for index,name in enumerate(names):
+        if name == 'Properties':
+            min_col, min_row,max_col,max_row=ranges[index]
+            data_row = 3
+            entry = []
+            for cells in sheet.iter_cols(min_col=min_col,min_row=data_row, max_col=max_col, max_row=data_row):
+                for cell in cells:
+                    entry.append(cell.value)
+            print("excel data: ",entry)
+         
+            
+           
+
+            if entry[columns[index].index('ExerciseId')]!= Exercise_Id:
+                entry[columns[index].index('ExerciseId')] = Exercise_Id
+                Edit_Excel = True
+            else:
+                pass
+            #check whether entry exists
+            sqlcommand = 'SELECT * FROM [CalstContent].[dbo].[Properties] where Id = ' + str(entry[columns[index].index('Id')]) + ' and ExerciseId = '+ str(entry[columns[index].index('ExerciseId')])
+            print(sqlcommand)
+            cursor.execute(sqlcommand)
+            list = cursor.fetchall()
+            print(list)
+            if not list:
+                Create_Entry = True
+                Edit_Excel = True
+                print(Create_Entry)
+            else:
+                print('db entry exists')
+
+            if Create_Entry:
+                cursor.execute('SELECT MAX(Id) AS maximum FROM Properties')
+                Id = cursor.fetchall()[0][0]+1
+                print(Id)
+                entry[columns[index].index('Id')] = Id
+
+                Id,WrapperId,ExerciseId,WordId,PronunciationId,Value,Key,Description,DialectId,Language_Id = entry
+                sqlcommand ='INSERT INTO [dbo].[Properties] VALUES([Id],[WrapperId],[ExerciseId],[WordId],[PronunciationId],[Value],[Key],[Description],[DialectId],[Language_Id])' 
+
+                for id in columns[index]:
+                    sqlcommand = sqlcommand.replace('['+id+']','?')
+                print(sqlcommand)
+                cursor.execute(sqlcommand,Id,WrapperId,ExerciseId,WordId,PronunciationId,Value,Key,Description,DialectId,Language_Id)
+                
+                cnxn.commit()
+            print(Edit_Excel)
+            
+            if Edit_Excel:
+                for ind,ex in enumerate(entry):
+                    data_row = 3
+                    print(ind,ex)
+                    sheet.cell(row=data_row, column = min_col+ind).value = ex
+                
+            else:
+                pass
+            
+
+                
+
+    return Edit_Excel
+
+
+
+def main(folder,cursor, cnxn):
     path = Path(folder)
     print(path.parent)
     structure_path  = Path(folder+'\\lessons_structure.xlsx')
@@ -117,83 +260,24 @@ def main(folder,cursor):
         
         
         for row in to_submit:
+            #structure file changes
             Create_Entry, Wrapper_Id, Exercise_Id = check_exerciseid_in_structure(wb_s=wb_s, cursor=cursor, row=row)
             if Create_Entry:
                 wb_s.save(filename=(str(structure_path))) 
             else:
                 pass
-            #open an exercise
+            #open an exercise.xlsx
+
             exercise_path = Path(sheet.cell(row=row, column=folder_col).value.replace('..',str(path.parent)))
             exercise_file = exercise_path/'exercise.xlsx'
             print(str(exercise_file))
             wb = load_workbook(str(exercise_file))
-            sheet = wb['Exercise']
+            Edit_Excel = work_on_exercises_sheet(wb=wb,cursor=cursor, cnxn=cnxn, Wrapper_Id=Wrapper_Id,Exercise_Id=Exercise_Id)
 
-            names, columns,ranges = get_sheet_structure(sheet=sheet)
-            print(names, columns,ranges)
-            #check WrapperExercises values
-            min_col,min_row, max_col, max_row = ranges[names.index('WrapperExercises')]
-            ids = columns[names.index('WrapperExercises')]
-
-            values = []
-            for cells in sheet.iter_cols(min_col=min_col,min_row=3, max_col=max_col, max_row=3):
-                for cell in cells:
-                    values.append((cell.value,cell.column))
-            print(values)
-            Edit_Excel = False
-            if values[ids.index('Wrapper_Id')][0] != Wrapper_Id:
-                print('wrapper_id', str(Wrapper_Id),values[ids.index('Wrapper_Id')][0])
-                sheet.cell(row=3, column=values[ids.index('Wrapper_Id')][1]).value=Wrapper_Id
-                Edit_Excel = True
-            else:
-                pass
-            if values[ids.index('Exercise_Id')][0] != Exercise_Id:
-                print('exercise_id', str(Exercise_Id),values[ids.index('Exercise_Id')][0])
-                sheet.cell(row=3, column=values[ids.index('Exercise_Id')][1]).value = Exercise_Id
-                Edit_Excel = True
-            else:
-                pass
-            
-            min_col,min_row, max_col, max_row = ranges[names.index('Exercises')]
-            ids = columns[names.index('Exercises')]
-
-            values = []
-            for cells in sheet.iter_cols(min_col=min_col,min_row=3, max_col=max_col, max_row=3):
-                for cell in cells:
-                    values.append((cell.value,cell.column))
-            print(values)
-        
-           
-            if values[ids.index('Id')][0] != Exercise_Id:
-                print('exercise_id', str(Exercise_Id),values[ids.index('Id')][0])
-                sheet.cell(row=3, column=values[ids.index('Id')][1]).value = Exercise_Id
-                Edit_Excel = True
-            else:
-                pass
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
             if Edit_Excel:
-               wb.save(str(exercise_file))
+                 wb.save(str(exercise_file))
             else:
                 pass
-            
-
-            
-
-
-            
-    
-
-          
-               
 
 
 
@@ -209,5 +293,5 @@ if __name__ == "__main__":
     cursor = cnxn.cursor()
     folder = 'C:\\Source\\Repos\\mysql-excel\\Spanish_course_styled\\'
     #folder = 'G:\\My Drive\\CALST_courses\\Spanish_course_styled\\'
-    main(folder=folder, cursor=cursor)
+    main(folder=folder, cursor=cursor, cnxn=cnxn)
     cnxn.commit()
